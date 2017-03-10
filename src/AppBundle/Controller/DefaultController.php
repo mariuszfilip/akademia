db354ae83f1845416e9753bf1c82a4e445a1f1e1
LysanderPL <podania.12@gmail.com>
2017-03-10 17:46:48 +0100

    formatowanie

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -22,23 +22,24 @@ class DefaultController extends Controller
         $page->setDescription("Desc");
 
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-            'page'=>$page
+            'base_dir' => realpath($this->getParameter('kernel.root_dir') . '/..') . DIRECTORY_SEPARATOR,
+            'page' => $page
         ]);
     }
 
     /**
      * @Route("/show/{id}",name="show")
      */
-    public function showAction(Request $request,$id){
+    public function showAction(Request $request, $id)
+    {
         $page = new PageInfoDescription();
-        $page->setTitle("Title : ".$id);
+        $page->setTitle("Title : " . $id);
         $page->setDescription("Desc");
 
 
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-            'page'=>$page
+            'base_dir' => realpath($this->getParameter('kernel.root_dir') . '/..') . DIRECTORY_SEPARATOR,
+            'page' => $page
         ]);
     }
 }
